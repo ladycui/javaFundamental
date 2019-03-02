855f9e952c0bc9056bcaf86063f9619eebd330a5
ladycui <ladycui@126.com>
2019-03-03 00:28:53 +0800

    create a project

---
+++ src/main/java/com/cyn/concurrence/MutiThread.java
@@ -0,0 +1,47 @@
+package com.cyn.concurrence;
+
+/*
+*
+* */
+public class MutiThread {
+    private static int num = 0;
+
+    public synchronized void printNum(String tag) {
+        try {
+            if (tag.equals("a")) {
+                num = 100;
+                System.out.println("tag a, set num over! num: " + num);
+                Thread.sleep(1000);
+            } else {
+                num = 200;
+                System.out.println("tag b, set num over!, num: " + num);
+            }
+            System.out.println("tag " + tag + ", num = " + num);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        final MutiThread m1 = new MutiThread();
+        final MutiThread m2 = new MutiThread();
+
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                m1.printNum("a");
+            }
+        });
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                m2.printNum("b");
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+    }
+
+}
